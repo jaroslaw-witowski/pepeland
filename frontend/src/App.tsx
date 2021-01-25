81b833a70a9afa8607f9c72d158d8d0c81d6c87a
jaroslaw-witowski <jar.witowski@gmail.com>
2021-01-25 09:57:10 +0100

    Initial frontend commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      Initial frontend
+    </div>
+  );
+}
+
+export default App;
